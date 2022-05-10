57f2bd6452a496d15c06d2434cf400b7106c16ad
Anjan Koradia <anjankoradia2101@gmail.com>
2022-05-10 18:38:47 +0530

    modify validation criteria

--- validator/signupValidator.js
+++ validator/signupValidator.js
@@ -1,10 +1,10 @@
 const Joi = require("joi");
 
 const signupValidator = Joi.object({
-    username: Joi.string().pattern(new RegExp('^[a-z]')).min(4).required(),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]')).min(5).required(),
-    fname: Joi.string().required(),
-    lname: Joi.string().required(),
+    username: Joi.string().pattern(new RegExp('^[a-z]+$')).min(4).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]+$')).min(5).required(),
+    fname: Joi.string().pattern(new RegExp('^[a-zA-Z]+$')).required(),
+    lname: Joi.string().pattern(new RegExp('^[a-zA-Z]+$')).required(),
 });
 
 module.exports = signupValidator;
